24c4237887055597fcb70b4baadf40ac5488d320
sreedhar <sreedharks2003@gmail.com>
2022-06-18 12:59:44 +0530

    new features and models Project model pic field in Blog model

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 # Create your models here.
-tags=(('tech','#tech'),('engineering','#engineering'),('review','review'))
+tags=(('tech','#tech'),('engineering','#engineering'),('review','#review'))
 class Blog(models.Model):
     title=models.CharField(max_length=20)
     tag=models.CharField(choices=tags,default='tech',max_length=15)
+    pic=models.ImageField(default='media/postpics/default.jpg',upload_to='postpics',null=False)
     content=models.TextField() 
     def __str__(self):
         return self.title
     
+class Project(models.Model):
+    project_name=models.CharField(max_length=50)
+    pic=models.ImageField(default='media/postpics/default.jpg',upload_to='projectpics')
+
+    def __str__(self):
+        return self.project_name
--- sreedbackend/urls.py
+++ sreedbackend/urls.py
@@ -13,10 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from asyncio import set_child_watcher
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from graphene_django.views import GraphQLView
+from blog.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql',GraphQLView.as_view(graphiql=True))
+    path('',home,name='home'),
+    path('graphql',GraphQLView.as_view(graphiql=True),name='graphql')
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
