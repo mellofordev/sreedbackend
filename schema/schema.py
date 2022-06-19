8169ae0f3492843b7b85131660841f935e4d098b
sreedhar <sreedharks2003@gmail.com>
2022-06-19 13:57:52 +0530

    new schemas supporting projects,tag recommendation & single blog fetch

--- schema/schema.py
+++ schema/schema.py
@@ -1,16 +1,29 @@
-from dataclasses import field
+
 import graphene
-from graphene_django import DjangoObjectType
-from blog.models import Blog
+from graphene_django import DjangoObjectType,DjangoListField
+from blog.models import Blog, Project
 
 class BlogQ(DjangoObjectType):
     class Meta:
         model=Blog
-        fields=('id','title','tag','content')
+        fields=('id','title','tag','content','pic')
+class ProjectQ(DjangoObjectType):
+    class Meta:
+        model=Project
+        fields=('id','project_name','pic')
 class Query(graphene.ObjectType):
     blog_list=graphene.List(BlogQ)
-
+    node_blog=graphene.Field(BlogQ,id=graphene.Int())
+    tag_recommend=graphene.List(BlogQ,Tag=graphene.String())
+    projects=graphene.List(ProjectQ)
     def resolve_blog_list(root,info):
         return Blog.objects.all()
-
+    def resolve_node_blog(root,info,id):
+        print(root,info,id)
+        return Blog.objects.get(pk=id)
+    def resolve_tag_recommend(root,info,Tag):
+        print(Tag)
+        return Blog.objects.filter(tag=Tag)
+    def resolve_projects(root,info):
+        return Project.objects.all()
 schema =graphene.Schema(query=Query)
